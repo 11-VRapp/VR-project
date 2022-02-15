1bb57b82d1e57ad2816e39b1894ccedb52ba15b0
BONINI FRANCESCO s282890 <s282890@studenti.polito.it>
2022-02-15 14:25:00 +0100

    Aggiunto respawn se ci si allontana troppo

--- The RegnAnt/Assets/Scripts/WorldBundaryDeathCheck.cs
+++ The RegnAnt/Assets/Scripts/WorldBundaryDeathCheck.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class WorldBundaryDeathCheck : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Transform _player;
+    [SerializeField] private Transform _spawnPosition;
+    void OnTriggerEnter()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        _player.position = _spawnPosition.position;
+        _player.forward = _spawnPosition.forward;
     }
 }
