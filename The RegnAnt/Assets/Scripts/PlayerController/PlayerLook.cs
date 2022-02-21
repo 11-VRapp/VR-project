c3c9d2fca3cd15e2511a41e794962d5d6250d70d
BONINI FRANCESCO s282890 <s282890@studenti.polito.it>
2022-02-21 17:18:59 +0100

    Pulizie varie

--- The RegnAnt/Assets/Scripts/PlayerController/PlayerLook.cs
+++ The RegnAnt/Assets/Scripts/PlayerController/PlayerLook.cs
@@ -21,15 +21,6 @@ public class PlayerLook : MonoBehaviour
     private float xRotation;
     private float yRotation;
 
-    [SerializeField] private Transform _viewPosition;
-
-    //[SerializeField] private GameObject cursor;
-    //[SerializeField] private GameObject cursor_selected;
-    private Outline _lastHit;
-    public RaycastHit hit;
-
-    //public bool interactng = false; 
-
     private void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
@@ -47,51 +38,6 @@ public class PlayerLook : MonoBehaviour
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
         cam.transform.localRotation = Quaternion.Euler(xRotation, yRotation, cam.transform.rotation.z);
-        orientation.transform.localEulerAngles = new Vector3(orientation.transform.localEulerAngles.x, yRotation, orientation.transform.localEulerAngles.z);
-
-        //detectLookingObject();
-    }
-
-    /*private void detectLookingObject()
-    {
-        Debug.DrawLine(_viewPosition.position, _viewPosition.position + 2 * _viewPosition.forward, Color.magenta);
-        if (Physics.SphereCast(_viewPosition.position, 0.2f, _viewPosition.forward, out hit, 1f))
-        {
-            if (_lastHit != null && hit.transform != _lastHit.transform)
-            {
-                _lastHit.outlineWidth = 1f;
-                _lastHit.UpdateMaterialProperties();
-            }
-
-            _lastHit = hit.collider.GetComponent<Outline>();
-            if (_lastHit != null)
-            {
-                cursor.SetActive(false);
-                cursor_selected.SetActive(true);
-
-                _lastHit.outlineWidth = 10f;
-                _lastHit.UpdateMaterialProperties();
-            }
-            return;
-        }
-
-        if (hit.collider == null)
-        {
-            cursor.SetActive(true);
-            cursor_selected.SetActive(false);
-        }
-    }
-
-    public void Interact()
-    {
-        Debug.DrawLine(_viewPosition.position, _viewPosition.position + 2 * _viewPosition.forward, Color.magenta);
-        if (Physics.SphereCast(_viewPosition.position, 0.2f, _viewPosition.forward, out RaycastHit _hit, 2f, LayerMask.GetMask("Ant")))
-        {            
-            if(Input.GetKey(KeyCode.E))
-            {                
-                _hit.transform.GetComponent<DialogueTrigger>().TriggerDialogue();
-            }
-        }
-
-    }*/
+        orientation.transform.localEulerAngles = new Vector3(orientation.transform.localEulerAngles.x, yRotation, orientation.transform.localEulerAngles.z);      
+    }   
 }
